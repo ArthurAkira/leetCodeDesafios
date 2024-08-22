3a5e7a9391b32c5151d5c73eb6b558ee4066afe4
Arthur Akira <arthur.akira.toyonaga@gmail.ckom>
2024-08-22 14:14:45 -0300

    novo codigo no two sums

--- twoSum.java
+++ twoSum.java
@@ -10,3 +10,25 @@ class Solution {
         return new int[] {};
     }
 }
+
+/*Outra solucao
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        for (int i = 0; i < nums.length; i++) {
+            int j = i + 1;
+            if (nums[i] + nums[j] == target) {
+                    return new int[] { i, j };
+            }
+            else{
+                
+                for (; j < nums.length; j++) {
+                    if (nums[i] + nums[j] == target) {
+                        return new int[] { i, j };
+                    }
+                }
+            }
+        }
+        throw new IllegalArgumentException("No two sum solution");
+    }
+}
+*/
